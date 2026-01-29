e0163fc6bb584c290e21125bfd7e6f90415cbb26
nokariyu <muzafarhafidz347@gmail.com>
2026-01-30 04:49:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 APP_NAME="RiyuCalc"
 APP_DIR="$PREFIX/share/riyucalc"
-BIN_NAME="Rcalc"
+BIN_NAME="rc"
 REPO_RAW="https://raw.githubusercontent.com/nokariyu/RiyuCalcTerminal/main"
 
 echo "[*] Installing $APP_NAME..."
@@ -16,8 +16,8 @@ fi
 mkdir -p "$APP_DIR"
 
 echo "[*] Downloading program files..."
-curl -fsSL "$REPO_RAW/RiyuCalc-1.0.jar" -o "$APP_DIR/RiyuCalc-1.0.jar"
-curl -fsSL "$REPO_RAW/Rcalc" -o "$PREFIX/bin/$BIN_NAME"
+curl -fsSL "$REPO_RAW/RiyuCalc-1.1.jar" -o "$APP_DIR/RiyuCalc-1.1.jar"
+curl -fsSL "$REPO_RAW/rc" -o "$PREFIX/bin/$BIN_NAME"
 
 chmod +x "$PREFIX/bin/$BIN_NAME"
 
